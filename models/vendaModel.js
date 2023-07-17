b65bafcbf5a395793eb3d4a024b8c1a94fe7d46d
linuser1990 <linuxserver@hotmail.com>
2023-07-17 12:22:21 -0400

    links na home de compartilhamento via whatsapp, tanto para um contato especifico como para escolher qua contato

--- models/vendaModel.js
+++ models/vendaModel.js
@@ -268,9 +268,6 @@ const verificaPrecoVenda = async (req, res) => {
       listaDeObjetos[index].subtotal =parseFloat(listaDeObjetos[index].subtotal ) + parseFloat(rows[0].precovenda)
       listaDeObjetos[index].qtd = parseInt(listaDeObjetos[index].qtd) + 1
 
-      console.log('total '+total)
-      console.log('qtd '+listaDeObjetos[index].qtd)
-
    } else {
       total -= parseFloat(rows[0].precovenda)
       listaDeObjetos[index].subtotal =parseFloat(listaDeObjetos[index].subtotal ) -  parseFloat(rows[0].precovenda)
